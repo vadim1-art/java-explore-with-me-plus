1790e62a5f93860a03d6a1294c2415d5fa909c21
Katy <89160388957@mail.ru>
2026-08-11 22:17:42 +0300

    HTTP Client + dto

---
+++ stats/stats-client/src/main/java/ru/practicum/StatsClientConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class StatsClientConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
