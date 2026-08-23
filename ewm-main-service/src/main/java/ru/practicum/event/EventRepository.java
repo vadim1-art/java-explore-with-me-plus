03f8c9990a6b599ee80abd18bf452faa946b6c84
Egorka <egorka.fggg@yandex.ru>
2026-08-23 20:47:56 +0300

    add serviceAdmin2

--- ewm-main-service/src/main/java/ru/practicum/event/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
     // 1. Для Private API: Получить все события, созданные конкретным пользователем (с пагинацией)
     Page<Event> findAllByInitiatorId(Long initiatorId, Pageable pageable);
