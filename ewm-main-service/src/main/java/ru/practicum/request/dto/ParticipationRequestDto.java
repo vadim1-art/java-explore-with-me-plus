8137b22b22bfbd36da00ff4a0265984f7a275612
Katy <89160388957@mail.ru>
2026-08-28 15:31:14 +0300

    ошибки постман

--- ewm-main-service/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
+++ ewm-main-service/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class ParticipationRequestDto {
     private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("created")
     private LocalDateTime createdDate;
 
     private Long event;
